6ca01643f006936167095670161c1f21c5c45254
Thomas Spradling <thomasspradling13@gmail.com>
2023-05-29 15:34:31 -0700

    Added static files

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
 
 app.use('/api', router);
 
